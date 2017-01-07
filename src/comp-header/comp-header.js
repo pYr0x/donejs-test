b5cf0f96fe4d0ac2d6985c850d8c1c07ee0305e0
Julian <julian@MacBook-Pro.local>
2017-01-07 20:49:45 +0100

    Initial commit

---
+++ src/comp-header/comp-header.js
@@ -0,0 +1,22 @@
+import Component from 'can-component';
+import DefineMap from 'can-define/map/';
+import './comp-header.less';
+import view from './comp-header.stache';
+
+export const ViewModel = DefineMap.extend({
+  message: {
+    value: 'This is the comp-header component'
+  }
+});
+
+export default Component.extend({
+  tag: 'comp-header',
+  viewModel: ViewModel,
+  view: view,
+	events: {
+    init: function () {
+        console.log("test");
+        console.log(this.viewModel.foo.bar);
+		}
+	}
+});
